65053620ac4afd85d010beeea4622d32a620d306
Anirudh-Gupta <anirudh@nagakiran.(none)>
2013-12-04 15:54:16 +0530

    restoring refinery cms

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleVlc::Application.config.secret_token = 'dddb2b0bc222ff35a3a446b5801f3ea12410ddb66ad15b1df79dfd98604602e7c239c3c3d42cda287be00e7e6859c5bf2160716f34670428ea1bbe95302a86d0'
