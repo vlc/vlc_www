89c03ec16197e2732ae45e72ff1f99e48bf04c6b
Anirudh-Gupta <anirudh@nagakiran.(none)>
2013-12-24 17:54:43 +0530

    trying to fix heroku deploy issue

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-config.assets.initialize_on_precompile = false
+
 module VLC
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
