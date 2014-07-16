0ae9cb93b4e4421133c5a33b79abc8ec384a7967
Balakrishna <balakrishna@carmatec.com>
2014-07-16 11:04:54 +0530

    dynamic menu changes

--- config/initializers/refinery/menus.rb
+++ config/initializers/refinery/menus.rb
@@ -1,9 +1,9 @@
 Refinery::Menus.configure do |config|
 	# Configure which menus that will be created when seeding. You can then call them by refinery_menu('<MENU NAME>')
-  config.default_menus = ["sidebar_menu"]
+  # config.default_menus = ["sidebar_menu"] 
 
   # Configure whether to enable html attributes for menu items
-   config.html_attributes = true
+  # config.html_attributes = true
 
 
   # Configure which resources that can be linked to in the menu. Default is pages, and resources (files). 
@@ -14,5 +14,5 @@ Refinery::Menus.configure do |config|
   # title_attr: attribute name (or method name) on resource to be shown as its title
   # admin_page_filter: hash of conditions to be used for filtering objects shown to be add-able via menu edit page
   
-  # config.default_menus = {:refinery_page=>{:klass=>"Refinery::Page", :title_attr=>"title", :scope=>#<Proc:0x9fe5804@/home/Anirudh/codebase/VLC/vendor/bundle/ruby/1.9.1/gems/refinerycms-menus-2.0.7/lib/refinery/menus/configuration.rb:19>}, :refinery_resource=>{:klass=>"Refinery::Resource", :title_attr=>"file_name"}} 
+  # config.default_menus = {:refinery_page=>{:klass=>"Refinery::Page", :title_attr=>"title", :scope=>#<Proc:0xb73b760@/home/balakrishna/.rvm/gems/ruby-1.9.3-p545/gems/refinerycms-menus-2.0.7/lib/refinery/menus/configuration.rb:19>}, :refinery_resource=>{:klass=>"Refinery::Resource", :title_attr=>"file_name"}} 
 end
